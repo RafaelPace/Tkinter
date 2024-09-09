d0dd91c495073375ebe96d37945202d37466c9e5
Rafael Pace <58235970+RafaelPace@users.noreply.github.com>
2024-09-09 16:22:42 -0300

    projeto7.py

---
+++ projeto7.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import ttk
+
+def get_pos(event):
+    print(f'x: {event.x} y: {event.y}')
+
+# window
+window = tk.Tk()
+window.geometry('600x500')
+window.title('Event Binding')
+
+# widgets
+text = tk.Text(window)
+text.pack()
+
+entry = ttk.Entry(window)
+entry.pack()
+
+button = ttk.Button(window, text = 'A button')
+button.pack()
+
+# events
+# button.bind('<Alt-KeyPress-a>', lambda event: print(event))
+# window.bind('<KeyPress>', lambda event: print(f'a button was pressed ({event.char})'))
+
+# window.bind('<Motion>', get_pos)
+
+entry.bind('<FocusIn>', lambda event: print('entry field was selected'))
+
+text.bind('<Shift-MouseWheel>', lambda event: print("Mousewheel"))
+
+
+# run
+window.mainloop()
